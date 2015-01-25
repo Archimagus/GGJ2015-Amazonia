18f50f96077bfeb5c2ac8110b88777afa5f15373
michaelbianchini <mike@michaelbianchini.com>
2015-01-25 16:33:01 -0500

    lots

---
+++ GGJ Project/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour 
+{
+	public int levelToLoad = 0;
+	
+	public void LoadLevel()
+	{
+		Application.LoadLevel (levelToLoad);
+	}
+}
